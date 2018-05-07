66566a2cbabc2cc5dcfb075acbef511dba2e6b6f
jchhina2 <35819986+jchhina2@users.noreply.github.com>
2018-05-07 18:48:24 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 var map;
 function initMap(){
- map = new google.maps.Map(document.getElementById('map),{
+ map = new google.maps.Map(document.getElementById('map'),{
     center:{
       lat: 33.101,
       long: -96.677
