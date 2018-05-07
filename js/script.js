aac28e7f75cf8e8c143e44d051284c2f0a1096d7
jchhina2 <35819986+jchhina2@users.noreply.github.com>
2018-05-07 18:48:13 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
 var map;
+function initMap(){
+ map = new google.maps.Map(document.getElementById('map),{
+    center:{
+      lat: 33.101,
+      long: -96.677
+    },
+    zoom: 8
+ });                                               
+}
