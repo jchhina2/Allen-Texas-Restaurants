fdcab8419f5178423a455c4c96446d19d32ffa4a
jchhina2 <35819986+jchhina2@users.noreply.github.com>
2018-05-07 22:30:13 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,14 @@
 
 function initMap(){
-var map = new google.maps.Map(document.getElementById('map'),{
+var shinrai = {
+ info: '<strong>Shinrai Japanese Bistro</strong><br>\
+        541 W McDermott Drive, Allen, TX 75013<br>\
+         Category: Japanese<br>\
+         Comments: Yummy Bento Boxes!',
+ lat: 33.100279,
+ long: -96.676089
+};
+ var map = new google.maps.Map(document.getElementById('map'),{
  zoom: 13,   
  center: new google.maps.LatLng(lat: 33.101874, long: -96.676986),
  mapTypeId: google.maps.MapTypeId.ROADMAP   
