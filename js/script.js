1fe9e2c4de017d70ae6825f734ec0ac06b424e5e
Jasraj Chhina <35819986+jchhina2@users.noreply.github.com>
2018-07-19 18:02:28 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -25,6 +25,24 @@ function initMap(){
   lat: 33.099605,
   long: -96.680162
  };
+ 
+ var theBlueFish = {
+  info:'<strong>The Blue Fish</strong><br>\
+       940 Garden Park Dr, Allen, TX 75013<br>\
+       Category: Japanese/Sushi<br>\
+       Comments: A little pricey, but worth it!',
+  lat: 33.088590,
+  long: -96.684492
+ };
+ 
+ var froggCoffee = {
+  info:'<strong>Frogg Coffee Bar & Creperie</strong><br>\
+       832 Watters Creek Blvd, Allen, TX 75013<br>\
+       Category: Cafe<br>\
+       Comments: Quiet place with great crepes!',
+  lat: 33.088950,
+  long: -96.683832
+ }; 
  var map = new google.maps.Map(document.getElementById('map'),{
  zoom: 14,   
  center: new google.maps.LatLng(33.101874, -96.676986),
@@ -33,7 +51,9 @@ function initMap(){
  
  var locations = [[shinrai.info, shinrai.lat, shinrai.long, 0],
                  [thaiPan.info, thaiPan.lat, thaiPan.long, 1],
-                 [helloBoba.info, helloBoba.lat, helloBoba.long, 2],];
+                 [helloBoba.info, helloBoba.lat, helloBoba.long, 2],
+                 [theBlueFish.info, theBlueFish.lat, theBlueFish.long, 3],
+                 [froggCoffee.info, froggCoffee.lat, froggCoffee.long, 4],];
   
  
  var infowindow = new google.maps.InfoWindow({});
